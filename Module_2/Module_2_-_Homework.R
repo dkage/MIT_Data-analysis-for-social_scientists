35cb204d48327de905af0624cfdcc7f5202da01e
Danilo Cunha <danilokage@gmail.com>
2020-02-17 23:51:02 -0300

    Fix structure and add other answers to module_2

--- Module_2/Module_2_-_Homework.R
+++ Module_2/Module_2_-_Homework.R
@@ -1,16 +1,22 @@
+
 rm(list = ls())
 
-x<-rbinom(8, 1000, 0.2)
+
 #where n refers to number of observations, size refers to number of trials, 
 #and prob refers to probability of success on each trial
-hist(x)
 
 
+######################################################
+######################################################
+######################################################
 #Question 9&10
-_____(successes)
+successes<-rbinom(8, 1000, 0.2)
+hist(successes)
 
 
-
+######################################################
+######################################################
+######################################################
 #Question 12
 #probability of getting exactly 7 heads on 10 flips
 #dbinom(x, size, prob) where x is the observations, 
@@ -19,24 +25,33 @@ _____(successes)
 #size is the number of trials, prob is the probability
 
 #Part A
-# _______
+dbinom(7, 10, 0.65)
 #Part B
-
+1 - (dbinom(8, 10, 0.65) + dbinom(9, 10, 0.65) + dbinom(10, 10, 0.65))
 #Part C
+1 - (dbinom(0, 10, 0.65) + dbinom(1, 10, 0.65) + dbinom(2, 10, 0.65) + dbinom(3, 10, 0.65) + dbinom(4, 10, 0.65) + dbinom(5, 10, 0.65))
 
 
+######################################################
+######################################################
+######################################################
 #Question 14
+library(ggplot2)
+library(tidyverse)
 binom_draws <- as_tibble(data.frame(successes))
 
 estimated_pf <- binom_draws %>%
-  group_by(______) %>%
-  _____(n=n()) %>%
-  mutate(freq=n/sum(______))
+  group_by(successes) %>%
+  summarize(n=n()) %>%
+  mutate(freq=n/sum(n))
 
 ggplot(estimated_pf, aes(x=successes, y=freq)) +
   geom_col() +
   ylab("Estimated Density")
 
+######################################################
+######################################################
+######################################################
 #Question 15
 #Create a tibble with x and the analytical probability densities.
 my_binom<-as_tibble(list(x=0:n, prob=dbinom(0:n, n, p)))
@@ -53,9 +68,8 @@ ggplot(calculated_cdf, aes(x=x, y=cdf)) + geom_step() +
   ylab("CDF")
 
 
-1 - (dbinom(8, 10, 0.65) + dbinom(9, 10, 0.65) + dbinom(10, 10, 0.65))
-
-1 - (dbinom(0, 10, 0.65) + dbinom(1, 10, 0.65) + dbinom(2, 10, 0.65) + dbinom(3, 10, 0.65) + dbinom(4, 10, 0.65) + dbinom(5, 10, 0.65))
 
 
-runif(7, 3 ,8 )
+
+
+runif(7, 3, 8)
